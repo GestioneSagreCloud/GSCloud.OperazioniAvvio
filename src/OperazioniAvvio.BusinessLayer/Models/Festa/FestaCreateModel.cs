74bd9658cba9bf5f797821d8ce2e0c231c1a24ff
Angelo Pirola <angelo9871@gmail.com>
2026-06-30 13:19:14 +0200

    Add Festa model classes for create, update, and view
    Introduced FestaCreateModel, FestaModel, and FestaUpdateModel in the OperazioniAvvio.BusinessLayer.Models.Festa namespace. All models include DataInizio and DataFine properties; FestaModel also includes an Id property. The Id property is commented out in the create and update models.

---
+++ src/OperazioniAvvio.BusinessLayer/Models/Festa/FestaCreateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Models.Festa;
+
+public class FestaCreateModel
+{
+	//public int Id { get; set; }
+	public DateTime DataInizio { get; set; }
+	public DateTime DataFine { get; set; }
+}
