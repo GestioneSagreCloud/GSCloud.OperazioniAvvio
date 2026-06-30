ed37c2bbec06baa19cdf1e28804944c53f5d67d8
Angelo Pirola <angelo9871@gmail.com>
2026-06-30 13:20:02 +0200

    Add validation for Festa create and update models
    Introduced FestaCreateValidation and FestaUpdateValidation classes using FluentValidation. These ensure DataInizio is required and not in the past, and DataFine is required and after DataInizio.

---
+++ src/OperazioniAvvio.BusinessLayer/Validations/Festa/FestaCreateValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Validations.Festa;
+
+public class FestaCreateValidation : AbstractValidator<FestaCreateModel>
+{
+	public FestaCreateValidation()
+	{
+		RuleFor(x => x.DataInizio)
+			.NotEmpty().WithMessage("La data di inizio è obbligatoria.")
+			.GreaterThanOrEqualTo(DateTime.Today).WithMessage("La data di inizio deve essere oggi o in futuro.");
+
+		RuleFor(x => x.DataFine)
+			.NotEmpty().WithMessage("La data di fine è obbligatoria.")
+			.GreaterThan(x => x.DataInizio).WithMessage("La data di fine deve essere successiva alla data di inizio.");
+	}
+}
