665d576afb3da00e70aa430700bce4dca8c354bd
Angelo Pirola <angelo9871@gmail.com>
2026-06-30 13:19:32 +0200

    Add Intestazione model classes for create, update, and view
    Added IntestazioneCreateModel, IntestazioneModel, and IntestazioneUpdateModel in the OperazioniAvvio.BusinessLayer.Models.Intestazione namespace. These models encapsulate properties for managing "Intestazione" entities, supporting creation, update, and retrieval operations.

---
+++ src/OperazioniAvvio.BusinessLayer/Models/Intestazione/IntestazioneCreateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Models.Intestazione;
+
+public class IntestazioneCreateModel
+{
+	public int IdFesta { get; set; }
+	public string Titolo { get; set; } = null!;
+	public string Edizione { get; set; } = null!;
+	public string Luogo { get; set; } = null!;
+	public string Logo { get; set; } = null!;
+}
---
+++ src/OperazioniAvvio.BusinessLayer/Models/Intestazione/IntestazioneModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Models.Intestazione;
+
+public class IntestazioneModel
+{
+	public int Id { get; set; }
+	public int IdFesta { get; set; }
+	public string Titolo { get; set; } = null!;
+	public string Edizione { get; set; } = null!;
+	public string Luogo { get; set; } = null!;
+	public string Logo { get; set; } = null!;
+}
