cd2ca47af316326028ddccc05a25a703f738a06a
Angelo Pirola <angelo9871@gmail.com>
2026-06-30 13:20:31 +0200

    Add validation for Intestazione create and update models
    Introduce IntestazioneCreateValidation and IntestazioneUpdateValidation using FluentValidation. Enforce rules for IdFesta, Titolo, Edizione, and Luogo fields. Logo validation is included but commented out.

---
+++ src/OperazioniAvvio.BusinessLayer/Validations/Intestazione/IntestazioneCreateValidation.cs
@@ -0,0 +1,28 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Validations.Intestazione;
+
+public class IntestazioneCreateValidation : AbstractValidator<IntestazioneCreateModel>
+{
+	public IntestazioneCreateValidation()
+	{
+		RuleFor(x => x.IdFesta)
+			.GreaterThan(0).WithMessage("IdFesta must be greater than 0.");
+
+		RuleFor(x => x.Titolo)
+			.NotEmpty().WithMessage("Titolo is required.")
+			.MinimumLength(5).WithMessage("Titolo must be at least 5 characters.")
+			.MaximumLength(100).WithMessage("Titolo must be at most 100 characters.");
+
+		RuleFor(x => x.Edizione)
+			.NotEmpty().WithMessage("Edizione is required.")
+			.MinimumLength(5).WithMessage("Edizione must be at least 5 characters.")
+			.MaximumLength(200).WithMessage("Edizione must be at most 200 characters.");
+
+		RuleFor(x => x.Luogo)
+			.NotEmpty().WithMessage("Luogo is required.")
+			.MinimumLength(5).WithMessage("Luogo must be at least 5 characters.")
+			.MaximumLength(100).WithMessage("Luogo must be at most 100 characters.");
+
+		//RuleFor(x => x.Logo)
+		//	.NotEmpty().WithMessage("Logo is required.");
+	}
+}
