f286e316122ed8b174715502e08fa3ccd9a26056
Angelo Pirola <angelo9871@gmail.com>
2026-06-30 13:20:10 +0200

    Add validation for IdFesta in create and update models
    Added ImpostazioneCreateValidation and ImpostazioneUpdateValidation classes to ensure IdFesta is greater than 0 in both create and update operations, with custom error messages.

---
+++ src/OperazioniAvvio.BusinessLayer/Validations/Impostazione/ImpostazioneCreateValidation.cs
@@ -0,0 +1,10 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Validations.Impostazione;
+
+public class ImpostazioneCreateValidation : AbstractValidator<ImpostazioneCreateModel>
+{
+    public ImpostazioneCreateValidation()
+    {
+        RuleFor(x => x.IdFesta)
+            .GreaterThan(0).WithMessage("IdFesta must be greater than 0.");
+    }
+}
---
+++ src/OperazioniAvvio.BusinessLayer/Validations/Impostazione/ImpostazioneUpdateValidation.cs
@@ -0,0 +1,10 @@
+﻿namespace OperazioniAvvio.BusinessLayer.Validations.Impostazione;
+
+public class ImpostazioneUpdateValidation : AbstractValidator<ImpostazioneUpdateModel>
+{
+    public ImpostazioneUpdateValidation()
+    {
+        RuleFor(x => x.IdFesta)
+            .GreaterThan(0).WithMessage("IdFesta must be greater than 0.");
+    }
+}
